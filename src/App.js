9e1af39c7124b3a66f1a0ad6cdfa02c823f8e151
Poomsanthong <thanapoom.santhong10@gmail.com>
2025-10-13 00:28:19 +0200

    APi and movie container setted

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import searchIcon from "./search.svg";
 
-function App() {
+const API_URL = "https://omdbapi.com/?apikey=45cbd782&s=batman";
+const movie1 = {
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BYzU3ZjE3M2UtM2E4Ni00MDI5LTkyZGUtOTFkMGIyYjNjZGU3XkEyXkFqcGc@._V1_SX300.jpg",
+  Title: "Batman & Robin",
+  Type: "movie",
+  Year: "1997",
+  imdbID: "tt0118688",
+};
+const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&t=${title}`);
+    const data = await response.json();
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("batman");
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Movie Search</h1>
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Search for a movie..."
+          value="Men"
+          onClick={() => {}}
+        />
+        <img src={searchIcon} alt="Search" onClick={() => {}} />
+      </div>
+      <div className="container">{/* // Movie card */}</div>
     </div>
   );
-}
-
+};
 export default App;
